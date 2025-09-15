4903c12c4db532ae98c3c49387a761e282915f5b
rachitneedcse <rachitguptacse.098@gmail.com>
2025-09-15 11:03:42 +0530

    Final

--- linear_regression.py
+++ linear_regression.py
@@ -15,12 +15,28 @@ class LinearRegression:
 
             y_pred=np.dot(x,self.weights)+self.bias
 
-            dw=(1/n) *np.dot((y_pred-y),x.T)
+            dw=(1/n) *np.dot(x.T,(y_pred-y))
             db=(1/n) *np.sum(y_pred-y)
 
-            self.weights-=dw
-            self.bias-=db
+            self.weights-=self.learning_rate*dw
+            self.bias-=self.learning_rate*db
     
+    def predict(self,x):
+        return np.dot(x,self.weights)+self.bias
+    @staticmethod
+    def mse(y_pred,y):
+        return np.mean((y_pred-y)**2)
+    
+X=np.array([
+    [1],[2],[3]
+])
+Y=np.array([1,2,3])
+
+for lr in [0.1,0.2,0.01,0.3,0.15]:
+    model=LinearRegression(lr,500)
+    model.fit_train(X,Y)
+    y_pred=model.predict(X)
+    print(f"Learning Rate: {lr} Prediction : {y_pred} MSE:{model.mse(y_pred,Y)}")
         
     
 
